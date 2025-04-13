8cb883fe2579901518e7b93fdbbaaef0c9836ca0
Hrshit31 <66057088+ProPrime332@users.noreply.github.com>
2025-04-13 17:20:21 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,172 +1,185 @@
-    #include <pybind11/pybind11.h>
-    #include <pybind11/stl.h>
-    #include <iostream>
-    #include <vector>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <iostream>
+#include <vector>
+#include <functional>
 
-    namespace py = pybind11;
-    using namespace std;
+namespace py = pybind11;
+using namespace std;
 
-    struct CacheBlock {
-        int tag;
-        bool valid;
-        bool dirty;
-        int accessCount;
-        int lastUsedTime;
+struct CacheBlock {
+    int tag;
+    bool valid;
+    bool dirty;
+    int accessCount;
+    int lastUsedTime;
 
-        CacheBlock() : tag(0), valid(false), dirty(false), accessCount(0), lastUsedTime(0) {}
-        CacheBlock(int t, bool v, bool d, int ac, int lut) : tag(t), valid(v), dirty(d), accessCount(ac), lastUsedTime(lut) {}
-    };
+    CacheBlock() : tag(0), valid(false), dirty(false), accessCount(0), lastUsedTime(0) {}
+    CacheBlock(int t, bool v, bool d, int ac, int lut) : tag(t), valid(v), dirty(d), accessCount(ac), lastUsedTime(lut) {}
+};
 
-    struct CacheSet {
-        vector<CacheBlock> blocks;
-        CacheSet(int associativity) {
-            blocks.resize(associativity);
-        }
-    };
+struct CacheSet {
+    vector<CacheBlock> blocks;
+    CacheSet(int associativity) {
+        blocks.resize(associativity);
+    }
+};
 
-    class Cache {
-    public:
-        int numSets;
-        int associativity;
-        int blockSize;
-        vector<CacheSet> sets;
+class Cache {
+public:
+    int numSets;
+    int associativity;
+    int blockSize;
+    vector<CacheSet> sets;
 
-        Cache(int numSets, int associativity, int blockSize)
-            : numSets(numSets), associativity(associativity), blockSize(blockSize) {
-            sets.resize(numSets, CacheSet(associativity));
-        }
-
-        bool read(int address, int time);
-        bool write(int address, int time);
-        void replaceFIFO(int index, int tag);
-        void replaceLRU(int index, int tag, int time);
-        void replaceLFU(int index, int tag);
-    };
-
-    class MultiLevelCache {
-    public:
-        Cache L1, L2, L3;
-
-        MultiLevelCache()
-            : L1(64, 4, 64),
-            L2(256, 8, 64),
-            L3(1024, 16, 64) {}
-
-        bool accessMemory(int address, int time);
-    };
-
-    int getIndex(int address, int blockSize, int numSets) {
-        return (address / blockSize) % numSets;
+    Cache(int numSets, int associativity, int blockSize)
+        : numSets(numSets), associativity(associativity), blockSize(blockSize) {
+        sets.resize(numSets, CacheSet(associativity));
     }
 
-    int getTag(int address, int blockSize, int numSets) {
-        return address / (blockSize * numSets);
-    }
+    bool read(int address, int time, std::function<void(std::string)> callback);
+    bool write(int address, int time, std::function<void(std::string)> callback);
+    void replaceFIFO(int index, int tag);
+    void replaceLRU(int index, int tag, int time);
+    void replaceLFU(int index, int tag);
+};
 
-    bool Cache::read(int address, int time) {
-        int index = getIndex(address, blockSize, numSets);
-        int tag = getTag(address, blockSize, numSets);
+class MultiLevelCache {
+public:
+    Cache L1, L2, L3;
 
-        for (CacheBlock &block : sets[index].blocks) {
-            if (block.valid && block.tag == tag) {
-                cout << "HIT ✅ in Cache at index " << index << "\n";
-                block.lastUsedTime = time;
-                block.accessCount++;
-                return true;
-            }
-        }
+    MultiLevelCache()
+        : L1(64, 4, 64),
+          L2(256, 8, 64),
+          L3(1024, 16, 64) {}
 
-        replaceLRU(index, tag, time);
-        return false;
-    }
+    bool accessMemory(int address, int time, std::function<void(std::string)> callback);
+};
 
-    bool Cache::write(int address, int time) {
-        int index = getIndex(address, blockSize, numSets);
-        int tag = getTag(address, blockSize, numSets);
+int getIndex(int address, int blockSize, int numSets) {
+    return (address / blockSize) % numSets;
+}
 
-        for (CacheBlock &block : sets[index].blocks) {
-            if (block.valid && block.tag == tag) {
-                cout << "HIT ✅ Writing to Cache at index " << index << "\n";
-                block.lastUsedTime = time;
-                block.accessCount++;
-                block.dirty = true;
-                return true;
-            }
-        }
+int getTag(int address, int blockSize, int numSets) {
+    return address / (blockSize * numSets);
+}
 
-        replaceLRU(index, tag, time);
-        return false;
-    }
+bool Cache::read(int address, int time, std::function<void(std::string)> callback) {
+    int index = getIndex(address, blockSize, numSets);
+    int tag = getTag(address, blockSize, numSets);
 
-    void Cache::replaceFIFO(int index, int tag) {
-        for (CacheBlock &block : sets[index].blocks) {
-            if (!block.valid) {
-                block = CacheBlock(tag, true, false, 0, 0);
-                return;
-            }
-        }
-        sets[index].blocks.erase(sets[index].blocks.begin());
-        sets[index].blocks.push_back(CacheBlock(tag, true, false, 0, 0));
-    }
-
-    void Cache::replaceLRU(int index, int tag, int time) {
-        for (CacheBlock &block : sets[index].blocks) {
-            if (!block.valid) {
-                block = CacheBlock(tag, true, false, 0, time);
-                return;
-            }
-        }
-
-        int lruIndex = 0, oldestTime = sets[index].blocks[0].lastUsedTime;
-        for (int i = 1; i < associativity; i++) {
-            if (sets[index].blocks[i].lastUsedTime < oldestTime) {
-                lruIndex = i;
-                oldestTime = sets[index].blocks[i].lastUsedTime;
-            }
-        }
-
-        sets[index].blocks[lruIndex] = CacheBlock(tag, true, false, 0, time);
-    }
-
-    void Cache::replaceLFU(int index, int tag) {
-        int lfuIndex = 0;
-        int leastUsed = sets[index].blocks[0].accessCount;
-
-        for (int i = 1; i < associativity; i++) {
-            if (sets[index].blocks[i].accessCount < leastUsed) {
-                lfuIndex = i;
-                leastUsed = sets[index].blocks[i].accessCount;
-            }
-        }
-
-        sets[index].blocks[lfuIndex] = CacheBlock(tag, true, false, 1, 0);
-    }
-
-    bool MultiLevelCache::accessMemory(int address, int time) {
-        if (L1.read(address, time)) return true;
-        if (L2.read(address, time)) {
-            cout << "Moving block from L2 → L1\n";
-            L1.replaceLRU(getIndex(address, L1.blockSize, L1.numSets), getTag(address, L1.blockSize, L1.numSets), time);
+    for (CacheBlock &block : sets[index].blocks) {
+        if (block.valid && block.tag == tag) {
+            std::string msg = "HIT ✅ in Cache at index " + std::to_string(index) + "\n";
+            callback(msg);  // Pass message back to Python
+            block.lastUsedTime = time;
+            block.accessCount++;
             return true;
         }
-        if (L3.read(address, time)) {
-            cout << "Moving block from L3 → L2 → L1\n";
-            L2.replaceLRU(getIndex(address, L2.blockSize, L2.numSets), getTag(address, L2.blockSize, L2.numSets), time);
-            L1.replaceLRU(getIndex(address, L1.blockSize, L1.numSets), getTag(address, L1.blockSize, L1.numSets), time);
+    }
+
+    replaceLRU(index, tag, time);
+    std::string msg = "MISS ❌ in Cache at index " + std::to_string(index) + "\n";
+    callback(msg);  // Pass message back to Python
+    return false;
+}
+
+bool Cache::write(int address, int time, std::function<void(std::string)> callback) {
+    int index = getIndex(address, blockSize, numSets);
+    int tag = getTag(address, blockSize, numSets);
+
+    for (CacheBlock &block : sets[index].blocks) {
+        if (block.valid && block.tag == tag) {
+            std::string msg = "HIT ✅ Writing to Cache at index " + std::to_string(index) + "\n";
+            callback(msg);  // Pass message back to Python
+            block.lastUsedTime = time;
+            block.accessCount++;
+            block.dirty = true;
             return true;
         }
-
-        cout << "MISS ❌ Loading from RAM to L3 → L2 → L1\n";
-        L3.replaceLRU(getIndex(address, L3.blockSize, L3.numSets), getTag(address, L3.blockSize, L3.numSets), time);
-        L2.replaceLRU(getIndex(address, L2.blockSize, L2.numSets), getTag(address, L2.blockSize, L2.numSets), time);
-        L1.replaceLRU(getIndex(address, L1.blockSize, L1.numSets), getTag(address, L1.blockSize, L1.numSets), time);
-
-        return false;
     }
 
-    // ⬇ pybind11 bindings
-    PYBIND11_MODULE(mylib, m) {
-        py::class_<MultiLevelCache>(m, "MultiLevelCache")
-            .def(py::init<>())
-            .def("accessMemory", &MultiLevelCache::accessMemory, "Access memory at address and time");
+    replaceLRU(index, tag, time);
+    std::string msg = "MISS ❌ Writing to Cache at index " + std::to_string(index) + "\n";
+    callback(msg);  // Pass message back to Python
+    return false;
+}
+
+void Cache::replaceFIFO(int index, int tag) {
+    for (CacheBlock &block : sets[index].blocks) {
+        if (!block.valid) {
+            block = CacheBlock(tag, true, false, 0, 0);
+            return;
+        }
     }
+    sets[index].blocks.erase(sets[index].blocks.begin());
+    sets[index].blocks.push_back(CacheBlock(tag, true, false, 0, 0));
+}
+
+void Cache::replaceLRU(int index, int tag, int time) {
+    for (CacheBlock &block : sets[index].blocks) {
+        if (!block.valid) {
+            block = CacheBlock(tag, true, false, 0, time);
+            return;
+        }
+    }
+
+    int lruIndex = 0, oldestTime = sets[index].blocks[0].lastUsedTime;
+    for (int i = 1; i < associativity; i++) {
+        if (sets[index].blocks[i].lastUsedTime < oldestTime) {
+            lruIndex = i;
+            oldestTime = sets[index].blocks[i].lastUsedTime;
+        }
+    }
+
+    sets[index].blocks[lruIndex] = CacheBlock(tag, true, false, 0, time);
+}
+
+void Cache::replaceLFU(int index, int tag) {
+    int lfuIndex = 0;
+    int leastUsed = sets[index].blocks[0].accessCount;
+
+    for (int i = 1; i < associativity; i++) {
+        if (sets[index].blocks[i].accessCount < leastUsed) {
+            lfuIndex = i;
+            leastUsed = sets[index].blocks[i].accessCount;
+        }
+    }
+
+    sets[index].blocks[lfuIndex] = CacheBlock(tag, true, false, 1, 0);
+}
+
+bool MultiLevelCache::accessMemory(int address, int time, std::function<void(std::string)> callback) {
+    bool result = L1.read(address, time, callback);
+    if (!result) result = L2.read(address, time, callback);
+    if (!result) result = L3.read(address, time, callback);
+    return result;
+}
+
+// ⬇ pybind11 bindings
+PYBIND11_MODULE(mylib, m) {
+    py::class_<CacheBlock>(m, "CacheBlock")
+        .def(py::init<>())
+        .def_readwrite("tag", &CacheBlock::tag)
+        .def_readwrite("valid", &CacheBlock::valid)
+        .def_readwrite("dirty", &CacheBlock::dirty)
+        .def_readwrite("accessCount", &CacheBlock::accessCount)
+        .def_readwrite("lastUsedTime", &CacheBlock::lastUsedTime);
+
+    py::class_<CacheSet>(m, "CacheSet")
+        .def(py::init<int>())
+        .def_readwrite("blocks", &CacheSet::blocks);
+
+    py::class_<Cache>(m, "Cache")
+        .def(py::init<int, int, int>())
+        .def("read", &Cache::read)
+        .def("write", &Cache::write)
+        .def("replaceFIFO", &Cache::replaceFIFO)
+        .def("replaceLRU", &Cache::replaceLRU)
+        .def("replaceLFU", &Cache::replaceLFU)
+        .def_readwrite("sets", &Cache::sets);
+
+    py::class_<MultiLevelCache>(m, "MultiLevelCache")
+        .def(py::init<>())
+        .def("accessMemory", &MultiLevelCache::accessMemory);
+}
--- main.py
+++ main.py
@@ -1,19 +1,117 @@
-import mylib
+import tkinter as tk
+from tkinter import scrolledtext
+import mylib  # This is your pybind11 C++ module
+import random
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-cache = mylib.MultiLevelCache()
+class CacheSimulatorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Cache Simulator")
 
-# Simulate a few memory accesses
-print("Accessing address 0x1000 at time 1:")
-cache.accessMemory(0x1000, 1)
+        # Set up the Cache Simulator
+        self.cache = mylib.MultiLevelCache()
 
-print("\nAccessing address 0x2000 at time 2:")
-cache.accessMemory(0x2000, 2)
+        # Set up the UI elements
+        self.create_widgets()
 
-print("\nAccessing address 0x1000 again at time 3:")
-cache.accessMemory(0x1000, 3)
+    def create_widgets(self):
+        # Create a frame for input controls
+        self.frame = tk.Frame(self.root)
+        self.frame.pack(padx=10, pady=10)
 
-print("\nAccessing address 0x4000 at time 4:")
-cache.accessMemory(0x4000, 4)
+        # Address input field
+        self.label_address = tk.Label(self.frame, text="Address:")
+        self.label_address.grid(row=0, column=0, padx=5, pady=5)
 
-print("\nAccessing address 0x9000 at time 5:")
-cache.accessMemory(0x9000, 5)
+        self.entry_address = tk.Entry(self.frame, width=20)
+        self.entry_address.grid(row=0, column=1, padx=5, pady=5)
+
+        # Time input field
+        self.label_time = tk.Label(self.frame, text="Time:")
+        self.label_time.grid(row=1, column=0, padx=5, pady=5)
+
+        self.entry_time = tk.Entry(self.frame, width=20)
+        self.entry_time.grid(row=1, column=1, padx=5, pady=5)
+
+        # Access button
+        self.button_access = tk.Button(self.frame, text="Access Memory", command=self.access_memory)
+        self.button_access.grid(row=2, column=0, columnspan=2, pady=10)
+
+        # Cache log area
+        self.label_log = tk.Label(self.root, text="Cache Log:")
+        self.label_log.pack(pady=5)
+
+        self.cache_log = scrolledtext.ScrolledText(self.root, width=50, height=15, wrap=tk.WORD)
+        self.cache_log.pack(padx=10, pady=10)
+
+        # Visualization frame
+        self.visualization_frame = tk.Frame(self.root)
+        self.visualization_frame.pack(padx=10, pady=10)
+
+        # Button for random address and time generation
+        self.button_generate = tk.Button(self.visualization_frame, text="Generate Random Access", command=self.generate_random_access)
+        self.button_generate.pack(side=tk.LEFT, padx=5, pady=5)
+
+        # Plot for visualizing cache hits vs misses
+        self.button_plot = tk.Button(self.visualization_frame, text="Plot Cache Hits vs Misses", command=self.plot_cache_hits)
+        self.button_plot.pack(side=tk.LEFT, padx=5, pady=5)
+
+    def access_memory(self):
+        # Get address and time input values
+        try:
+            address = int(self.entry_address.get(), 16)  # Address in hexadecimal format
+            time = int(self.entry_time.get())
+        except ValueError:
+            self.cache_log.insert(tk.END, "Invalid input! Please enter valid Address and Time.\n")
+            return
+
+        # Clear the previous log
+        self.cache_log.delete(1.0, tk.END)
+
+        # Callback function to log messages
+        def log_message(message):
+            self.cache_log.insert(tk.END, message)
+            self.cache_log.yview(tk.END)  # Scroll to the bottom
+
+        # Access memory via the MultiLevelCache object (this is where the C++ logic is invoked)
+        self.cache.accessMemory(address, time, log_message)
+
+    def generate_random_access(self):
+        """Generate a random address and time for cache access and show the result."""
+        address = random.randint(0, 0xFFFFF)  # Generate random address in hex
+        time = random.randint(0, 100)  # Random time between 0 and 100
+        self.entry_address.delete(0, tk.END)
+        self.entry_address.insert(tk.END, hex(address))  # Display address in hexadecimal
+        self.entry_time.delete(0, tk.END)
+        self.entry_time.insert(tk.END, time)  # Display random time
+
+        # Automatically access memory with the generated values
+        self.access_memory()
+
+    def plot_cache_hits(self):
+        """Plot cache hits vs misses using matplotlib."""
+        # Example data for plotting (you would replace this with actual hit/miss data)
+        cache_hits = random.randint(50, 150)
+        cache_misses = random.randint(50, 150)
+
+        # Plot the data
+        fig, ax = plt.subplots()
+        ax.bar(['Cache Hits', 'Cache Misses'], [cache_hits, cache_misses], color=['green', 'red'])
+
+        ax.set_title("Cache Hits vs Misses")
+        ax.set_ylabel("Count")
+        ax.set_xlabel("Cache Activity")
+
+        # Embed the plot in the Tkinter window
+        canvas = FigureCanvasTkAgg(fig, master=self.visualization_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+# Set up the main Tkinter window
+root = tk.Tk()
+app = CacheSimulatorApp(root)
+
+# Start the Tkinter main loop
+root.mainloop()
